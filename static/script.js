83a44fe5a73c6ff680d169607906f60d1c5d0531
Arturo Santiago-Rivera <asantiago@arsari.com>
2024-09-29 10:58:28 -0400

    testing app.py

--- static/script.js
+++ static/script.js
@@ -22,7 +22,7 @@ function previewBatches() {
       document.getElementById('loading').style.display = 'none';
 
       if (data.error) {
-        document.getElementById('response').innerHTML = `<div class="error-message">Error: ${data}</div>`;
+        document.getElementById('response').innerHTML = `<div class="error-message">Error: ${data.error}</div>`;
         return;
       }
 
